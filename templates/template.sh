8c5173492e9fc5c8887431897beb9149d2ead3ce
Louis Cordier <lcordier@gmail.com>
2021-01-24 18:19:41 +0200

    Latesh shell template.

--- templates/template.sh
+++ templates/template.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
 # https://betterdev.blog/minimal-safe-bash-script-template/
-
 set -Eeuo pipefail
-
-cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
-
 trap cleanup SIGINT SIGTERM ERR EXIT
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 usage() {
   cat <<EOF
-Usage: $(basename "$0") [-h] [-v] [-f] -p param_value arg1 [arg2...]
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-f] -p param_value arg1 [arg2...]
 
 Script description here.
 
@@ -31,9 +29,9 @@ cleanup() {
 
 setup_colors() {
   if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-    NOCOLOR='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
   else
-    NOCOLOR='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
   fi
 }
 
@@ -55,28 +53,16 @@ parse_params() {
 
   while :; do
     case "${1-}" in
-    -h | --help)
-      usage
-      ;;
-    -v | --verbose)
-      set -x
-      ;;
-    --no-color)
-      NO_COLOR=1
-      ;;
-    -f | --flag) # example flag
-      flag=1
-      ;;
+    -h | --help) usage ;;
+    -v | --verbose) set -x ;;
+    --no-color) NO_COLOR=1 ;;
+    -f | --flag) flag=1 ;; # example flag
     -p | --param) # example named parameter
       param="${2-}"
       shift
       ;;
-    -?*)
-      die "Unknown option: $1"
-      ;;
-    *)
-      break
-      ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
     esac
     shift
   done
@@ -95,9 +81,7 @@ setup_colors
 
 # script logic here
 
-msg "${RED}Read parameters:${NOCOLOR}"
+msg "${RED}Read parameters:${NOFORMAT}"
 msg "- flag: ${flag}"
 msg "- param: ${param}"
 msg "- arguments: ${args[*]-}"
-
-
